56b77b722ed5c08bc1fd2a80f18d9764a32845d2
Sean Karlage <skarlage@get9.io>
2016-02-11 01:59:33 -0500

    mostly working test program, need to debug further

--- test.cpp
+++ test.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
-#include "PGMReader.hpp"
+#include "io/PGMReader.hpp"
 #include "matrix/Matrix.hpp"
+
+using namespace sipl;
 
 int main(int argc, char** argv)
 {
-    const std::string filename = "test.pgm";
+    if (argc < 2) {
+        std::cout << "Usage:" << std::endl;
+        std::cout << "    " << argv[0] << " file.pgm" << std::endl;
+        std::exit(1);
+    }
+
+    const std::string filename{argv[1]};
     PGMReader reader{filename};
-    auto mat = reader.read();
+    auto mat = reader.read8();
     std::cout << mat({0, 0}) << std::endl;
 }
