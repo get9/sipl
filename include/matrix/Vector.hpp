395de05cf3584e3f4c1e5cdbb7856ab20801c2d0
Sean Karlage <skarlage@get9.io>
2016-02-11 22:54:30 -0500

    Add helpful aliases

--- include/matrix/Vector.hpp
+++ include/matrix/Vector.hpp
@@ -2,8 +2,12 @@
 
 namespace sipl
 {
+
+// Helpful defines for vector types
 template <typename T>
 using Vector3<T> = std::array<T, 3>;
 
 using Vector3b = typename Vector3<uint8_t>;
+
+using RGBPixel = typename Vector3b;
 }
