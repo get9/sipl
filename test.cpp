d3eda80dc7c988707bec7c18587f23859d9aa011
Sean Karlage <skarlage@get9.io>
2016-02-12 02:26:52 -0500

    Better test program for I/O - can choose write type

--- test.cpp
+++ test.cpp
@@ -2,21 +2,28 @@
 #include <algorithm>
 #include "matrix/Matrix.hpp"
 #include "io/PpmIO.hpp"
+#include "io/PgmIO.hpp"
 
 using namespace sipl;
 
 int main(int argc, char** argv)
 {
-    if (argc < 3) {
+    if (argc < 4) {
         std::cout << "Usage:" << std::endl;
-        std::cout << "    " << argv[0] << " infile.pgm outfile.pgm"
-                  << std::endl;
+        std::cout << "    " << argv[0]
+                  << " infile.pgm outfile.pgm [ascii/binary]" << std::endl;
         std::exit(1);
     }
 
-    // const std::string filename{argv[1]};
-    PpmIO ppm;
+    PgmIO ppm;
+    std::string write_type{argv[3]};
     auto mat = ppm.read(argv[1]);
-    ppm.write(mat, argv[2], PpmIO::PType::BINARY);
-    // ppm.write(mat, argv[2], PpmIO::PType::ASCII);
+    if (write_type == "ascii") {
+        ppm.write(mat, argv[2], PpmIO::PType::ASCII);
+    } else if (write_type == "binary") {
+        ppm.write(mat, argv[2], PgmIO::PType::BINARY);
+    } else {
+        std::cerr << "bad write type" << std::endl;
+        std::exit(1);
+    }
 }
