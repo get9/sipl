7ba2d6a98a2e4a8165b7e8ba1e27152085923860
Sean Karlage <skarlage@get9.io>
2016-02-23 00:48:06 -0500

    In special cases where the incoming x and y params are exact integers, then the floor/ceil calls fail to actually make ceil - floor = 1. Therefore, need to check for this special case using std::modf

--- include/improc/BilinearInterpolator.hpp
+++ include/improc/BilinearInterpolator.hpp
@@ -17,33 +17,34 @@ struct BilinearInterpolator {
                              const Dtype fill_value = Dtype(0))
     {
         // Get four pixel values closest to this
-        int32_t floor_row = int32_t(std::floor(y));
-        int32_t floor_col = int32_t(std::floor(x));
-        int32_t ceil_row = int32_t(std::ceil(y));
-        int32_t ceil_col = int32_t(std::ceil(x));
+        // Note: need to do this hackery in cases where x and y are exact
+        // integers
+        double d_col0, d_row0;
+        double xfrac = int32_t(std::modf(x, &d_col0));
+        double yfrac = int32_t(std::modf(y, &d_row0));
+        int32_t col0 = int32_t(d_col0);
+        int32_t row0 = int32_t(d_row0);
+        int32_t col1 = (xfrac == 0 ? col0 + 1 : int32_t(std::ceil(x)));
+        int32_t row1 = (yfrac == 0 ? row0 + 1 : int32_t(std::ceil(y)));
 
         // Set to 0 if these are outside the range
-        if (floor_row < 0 || floor_row >= img.dims[0] || floor_col < 0 ||
-            floor_col >= img.dims[1] || ceil_row < 0 ||
-            ceil_row >= img.dims[0] || ceil_col < 0 ||
-            ceil_col >= img.dims[1]) {
+        if (row0 < 0 || row0 >= img.dims[0] || col0 < 0 ||
+            col0 >= img.dims[1] || row1 < 0 || row1 >= img.dims[0] ||
+            col1 < 0 || col1 >= img.dims[1]) {
             return fill_value;
         }
 
         // Do first interpolation (along columns)
-        InternalType tmp_i1 = ((ceil_col - x) / (ceil_col - floor_col)) *
-                                  img(floor_row, floor_col) +
-                              ((x - floor_col) / (ceil_col - floor_col)) *
-                                  img(floor_row, ceil_col);
-        InternalType tmp_i2 = ((ceil_col - x) / (ceil_col - floor_col)) *
-                                  img(ceil_row, floor_col) +
-                              ((x - floor_col) / (ceil_col - floor_col)) *
-                                  img(ceil_row, ceil_col);
+        InternalType tmp_i1 = ((col1 - x) / (col1 - col0)) * img(row0, col0) +
+                              ((x - col0) / (col1 - col0)) * img(row0, col1);
+        InternalType tmp_i2 = ((col1 - x) / (col1 - col0)) * img(row1, col0) +
+                              ((x - col0) / (col1 - col0)) * img(row1, col1);
 
         // Then linearly interpolate those values
-        InternalType f = ((ceil_row - y) / (ceil_row - floor_row)) * tmp_i1 +
-                         ((y - floor_row) / (ceil_row - floor_row)) * tmp_i2;
+        InternalType f = ((row1 - y) / (row1 - row0)) * tmp_i1 +
+                         ((y - row0) / (row1 - row0)) * tmp_i2;
 
+        std::cout << f << std::endl;
         return clamp(f);
     }
 };
