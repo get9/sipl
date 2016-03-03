332393f03aeb8f39bb369eef4afbb3b2ff1cb014
Sean Karlage <skarlage@get9.io>
2016-03-03 12:37:49 -0500

    Add trait enforcement for scalar manipulation of vectors

--- include/matrix/VectorOps.hpp
+++ include/matrix/VectorOps.hpp
@@ -1,69 +1,87 @@
 #include "matrix/VectorBase.hpp"
 
 // Free-function operations
-template <typename VectorType, typename Scalar>
-VectorType operator/(
-    VectorType v,
-    typename std::enable_if<std::is_integral<Scalar>::value>::type s)
+template <typename T, int32_t L, typename C, typename Scalar>
+sipl::VectorBase<T, L, C> operator/(
+    sipl::VectorBase<T, L, C> v,
+    typename std::enable_if<std::is_arithmetic<Scalar>::value>::type s)
 {
     v /= s;
     return v;
 }
 
-template <typename VectorType, typename Scalar>
-VectorType operator*(VectorType v, Scalar s)
+template <typename T, int32_t L, typename C, typename Scalar>
+sipl::VectorBase<T, L, C> operator*(
+    sipl::VectorBase<T, L, C> v,
+    typename std::enable_if<std::is_arithmetic<Scalar>::value>::type s)
 {
     v *= s;
     return v;
 }
 
-template <typename VectorType, typename Scalar>
-VectorType operator*(Scalar s, VectorType v)
+template <typename T, int32_t L, typename C, typename Scalar>
+sipl::VectorBase<T, L, C> operator*(
+    typename std::enable_if<std::is_arithmetic<Scalar>::value>::type s,
+    sipl::VectorBase<T, L, C> v)
 {
     return v * s;
 }
 
-template <typename VectorType, typename Scalar>
-VectorType operator+(
-    VectorType v,
-    typename std::enable_if<std::is_integral<Scalar>::value>::type s)
+template <typename T, int32_t L, typename C, typename Scalar>
+sipl::VectorBase<T, L, C> operator+(
+    sipl::VectorBase<T, L, C> v,
+    typename std::enable_if<std::is_arithmetic<Scalar>::value>::type s)
 {
     v += s;
     return v;
 }
 
-template <typename VectorType, typename Scalar>
-VectorType operator+(
-    typename std::enable_if<std::is_integral<Scalar>::value>::type s,
-    VectorType v)
+template <typename T, int32_t L, typename C, typename Scalar>
+sipl::VectorBase<T, L, C> operator+(
+    typename std::enable_if<std::is_arithmetic<Scalar>::value>::type s,
+    sipl::VectorBase<T, L, C> v)
 {
     return s + v;
 }
 
-template <typename VectorType, typename Scalar>
-VectorType operator-(VectorType v, Scalar s)
+template <typename T, int32_t L, typename C, typename Scalar>
+sipl::VectorBase<T, L, C> operator-(
+    sipl::VectorBase<T, L, C> v,
+    typename std::enable_if<std::is_arithmetic<Scalar>::value>::type s)
 {
     v -= s;
     return v;
 }
 
 // Operators for vectors of different types
-template <typename VectorType1, typename VectorType2>
-VectorType1 operator+(const VectorType1& v1, const VectorType2& v2)
+template <typename T1,
+          int32_t L1,
+          typename C1,
+          typename T2,
+          int32_t L2,
+          typename C2>
+sipl::VectorBase<T1, L1, C1> operator+(const sipl::VectorBase<T1, L1, C1>& v1,
+                                       const sipl::VectorBase<T2, L2, C2>& v2)
 {
     assert(v1.size() == v2.size() && "size mismatch");
-    VectorType1 new_v(v1.size());
+    sipl::VectorBase<T1, L1, C1> new_v(v1.size());
     for (int32_t i = 0; i < v1.size(); ++i) {
         new_v[i] = v1[i] + v2[i];
     }
     return new_v;
 }
 
-template <typename VectorType1, typename VectorType2>
-VectorType1 operator-(const VectorType1& v1, const VectorType2& v2)
+template <typename T1,
+          int32_t L1,
+          typename C1,
+          typename T2,
+          int32_t L2,
+          typename C2>
+sipl::VectorBase<T1, L1, C1> operator-(const sipl::VectorBase<T1, L1, C1>& v1,
+                                       const sipl::VectorBase<T2, L2, C2>& v2)
 {
     assert(v1.size() == v2.size() && "size mismatch");
-    VectorType1 new_v(v1.size());
+    sipl::VectorBase<T1, L1, C1> new_v(v1.size());
     for (int32_t i = 0; i < v1.size(); ++i) {
         new_v[i] = v1[i] - v2[i];
     }
@@ -71,8 +89,14 @@ VectorType1 operator-(const VectorType1& v1, const VectorType2& v2)
 }
 
 // Comparison operators
-template <typename VectorType1, typename VectorType2>
-bool operator==(const VectorType1& lhs, const VectorType2& rhs)
+template <typename T1,
+          int32_t L1,
+          typename C1,
+          typename T2,
+          int32_t L2,
+          typename C2>
+bool operator==(const sipl::VectorBase<T1, L1, C1>& lhs,
+                const sipl::VectorBase<T2, L2, C2>& rhs)
 {
     assert(lhs.size() == rhs.size() && "size mismatch");
     for (int32_t i = 0; i < lhs.size(); ++i) {
@@ -83,33 +107,63 @@ bool operator==(const VectorType1& lhs, const VectorType2& rhs)
     return true;
 }
 
-template <typename VectorType1, typename VectorType2>
-bool operator!=(const VectorType1& lhs, const VectorType2& rhs)
+template <typename T1,
+          int32_t L1,
+          typename C1,
+          typename T2,
+          int32_t L2,
+          typename C2>
+bool operator!=(const sipl::VectorBase<T1, L1, C1>& lhs,
+                const sipl::VectorBase<T2, L2, C2>& rhs)
 {
     return !operator==(lhs, rhs);
 }
 
 // XXX Better to compare based on norm or
-template <typename VectorType1, typename VectorType2>
-bool operator<(const VectorType1& lhs, const VectorType2& rhs)
+template <typename T1,
+          int32_t L1,
+          typename C1,
+          typename T2,
+          int32_t L2,
+          typename C2>
+bool operator<(const sipl::VectorBase<T1, L1, C1>& lhs,
+               const sipl::VectorBase<T2, L2, C2>& rhs)
 {
     return lhs.norm() < rhs.norm();
 }
 
-template <typename VectorType1, typename VectorType2>
-bool operator>(const VectorType1& lhs, const VectorType2& rhs)
+template <typename T1,
+          int32_t L1,
+          typename C1,
+          typename T2,
+          int32_t L2,
+          typename C2>
+bool operator>(const sipl::VectorBase<T1, L1, C1>& lhs,
+               const sipl::VectorBase<T2, L2, C2>& rhs)
 {
     return operator<(rhs, lhs);
 }
 
-template <typename VectorType1, typename VectorType2>
-bool operator<=(const VectorType1& lhs, const VectorType2& rhs)
+template <typename T1,
+          int32_t L1,
+          typename C1,
+          typename T2,
+          int32_t L2,
+          typename C2>
+bool operator<=(const sipl::VectorBase<T1, L1, C1>& lhs,
+                const sipl::VectorBase<T2, L2, C2>& rhs)
 {
     return !operator>(lhs, rhs);
 }
 
-template <typename VectorType1, typename VectorType2>
-bool operator>=(const VectorType1& lhs, const VectorType2& rhs)
+template <typename T1,
+          int32_t L1,
+          typename C1,
+          typename T2,
+          int32_t L2,
+          typename C2>
+bool operator>=(const sipl::VectorBase<T1, L1, C1>& lhs,
+                const sipl::VectorBase<T2, L2, C2>& rhs)
 {
     return !operator<(lhs, rhs);
 }
--- vector_test.cpp
+++ vector_test.cpp
@@ -7,22 +7,23 @@ int main()
 {
     Vector3i v{1, 2, 3};
     VectorXi v2{2, 4, 6};
-    std::cout << "Static" << std::endl;
-    std::cout << v << std::endl;
-    auto new_v = v.apply_clone([](auto e) { return e << 1; });
-    std::cout << new_v << std::endl;
-    std::cout << v << std::endl;
-    v += 2;
-    std::cout << v << std::endl;
+    std::cout << "static:  " << v << std::endl;
+    std::cout << "dynamic: " << v2 << std::endl;
+    std::cout << "apply_clone: ";
+    std::cout << v.apply_clone([](auto e) { return e << 1; }) << std::endl;
 
     // Add vectors of different types
+    std::cout << "Math operations" << std::endl;
+    std::cout << v + v << std::endl;
+    std::cout << v2 + v << std::endl;
+    std::cout << v2 + v2 << std::endl;
     std::cout << v + v2 << std::endl;
+    std::cout << std::endl;
 
-    std::cout << "Dynamic" << std::endl;
-    std::cout << v2 << std::endl;
-    auto new_v2 = v.apply_clone([](auto e) { return e << 1; });
-    std::cout << new_v2 << std::endl;
-    std::cout << v2 << std::endl;
-    v2 += 2;
-    std::cout << v2 << std::endl;
+    std::cout << "Comparison operations" << std::endl;
+    std::cout << "operator==: " << (v == v2) << std::endl;
+    std::cout << "operator<: " << (v < v2) << std::endl;
+    std::cout << "operator>: " << (v > v2) << std::endl;
+    std::cout << "operator<=: " << (v <= v2) << std::endl;
+    std::cout << "operator>=: " << (v >= v2) << std::endl;
 }
