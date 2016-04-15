e7f56d1b52250d7c9f5256cfa393f078fedabfc2
Sean Karlage <skarlage@get9.io>
2016-04-15 00:05:52 -0400

    Add morphology routines

--- examples/pngtest.cpp
+++ examples/pngtest.cpp
@@ -1,7 +1,8 @@
-#include "io/PngIO.hpp"
-#include "io/PpmIO.hpp"
-#include "matrix/Statistics.hpp"
 #include "improc/Filter.hpp"
+#include "improc/Morphology.hpp"
+#include "io/PgmIO.hpp"
+#include "io/PngIO.hpp"
+#include "matrix/Statistics.hpp"
 #include <cstring>
 #include <iostream>
 #include <memory>
@@ -22,6 +23,8 @@ std::vector<std::string> generate_filenames(const std::string& format,
 
 std::vector<MatrixXb> read_video_dir(const std::vector<std::string>& filenames);
 
+std::vector<Vector2i> bresenham(const Vector2i& p1, const Vector2i& p2);
+
 int main(int argc, char** argv)
 {
     parse_commandline(argv);
@@ -30,12 +33,39 @@ int main(int argc, char** argv)
     auto filenames = generate_filenames(format, img_start, img_count);
     auto grays = read_video_dir(filenames);
 
-    // Compute median img
-    auto avg = median(grays);
-    std::cout << "done computing median" << std::endl;
-    auto diff = math::abs(grays[0] - avg).clip(0, 255).as_type<RgbPixel>();
+    // Compute average img
+    auto avg = average(grays);
 
-    PngIO::write(diff, "median.png");
+    // Compute sagittal view
+    auto points = bresenham({start_x, start_y}, {end_x, end_y});
+    MatrixXb slice_img(grays.size() * 2, points.size());
+    for (size_t i = 0; i < grays.size(); ++i) {
+
+        // Subtract background
+        auto diff = math::abs(grays[i] - avg).clip(0, 255).as_type<uint8_t>();
+
+        // Threshold away low 10%
+        auto thresh = threshold_binary(diff, uint8_t(0.1 * 255));
+        for (size_t p = 0; p < points.size(); ++p) {
+            slice_img(2 * i, p) = thresh(points[p][1], points[p][0]);
+            slice_img(2 * i + 1, p) = thresh(points[p][1], points[p][0]);
+        }
+    }
+
+    // Median blur
+    auto median_ksize = int32_t(0.02 * points.size());
+    if (median_ksize % 2 == 0) {
+        median_ksize += 1;
+    }
+    std::cout << "median filter size: " << median_ksize << std::endl;
+    slice_img = median_filter(slice_img, median_ksize, median_ksize);
+
+    // Dilate by square
+    auto morph_ksize = int32_t(0.04 * points.size());
+    slice_img = morphology::dilate(
+        slice_img, morphology::kernels::rectangle(morph_ksize, morph_ksize));
+
+    PgmIO::write(slice_img, "reslice.pgm");
 }
 
 void parse_commandline(char** argv)
@@ -72,10 +102,72 @@ std::vector<MatrixXb> read_video_dir(const std::vector<std::string>& filenames)
     gray_pngs.reserve(filenames.size());
     for (const auto& f : filenames) {
         auto color = PngIO::read(f);
-        std::cout << "read: " << f << std::endl;
         gray_pngs.push_back(color_to_grayscale(color));
     }
-    std::cout << "Done with reading" << std::endl;
 
     return gray_pngs;
+}
+
+// Implementation taken from:
+// https://www.cs.unm.edu/~angel/BOOK/INTERACTIVE_COMPUTER_GRAPHICS/FOURTH_EDITION/PROGRAMS/bresenham.c
+std::vector<Vector2i> bresenham(const Vector2i& p1, const Vector2i& p2)
+{
+    std::vector<Vector2i> points;
+    int32_t dx, dy, i, e;
+    int32_t incx, incy, inc1, inc2;
+    int32_t x, y;
+
+    dx = p2[0] - p1[0];
+    dy = p2[1] - p1[1];
+
+    if (dx < 0) {
+        dx = -dx;
+    }
+    if (dy < 0) {
+        dy = -dy;
+    }
+    incx = 1;
+    if (p2[0] < p1[0]) {
+        incx = -1;
+    }
+    incy = 1;
+    if (p2[1] < p1[1]) {
+        incy = -1;
+    }
+    x = p1[0];
+    y = p1[1];
+
+    if (dx > dy) {
+        points.push_back({x, y});
+        e = 2 * dy - dx;
+        inc1 = 2 * (dy - dx);
+        inc2 = 2 * dy;
+        for (i = 0; i < dx; i++) {
+            if (e >= 0) {
+                y += incy;
+                e += inc1;
+            } else {
+                e += inc2;
+            }
+            x += incx;
+            points.push_back({x, y});
+        }
+    } else {
+        points.push_back({x, y});
+        e = 2 * dx - dy;
+        inc1 = 2 * (dx - dy);
+        inc2 = 2 * dx;
+        for (i = 0; i < dy; i++) {
+            if (e >= 0) {
+                x += incx;
+                e += inc1;
+            } else {
+                e += inc2;
+            }
+            y += incy;
+            points.push_back({x, y});
+        }
+    }
+
+    return points;
 }
---
+++ include/improc/Morphology.hpp
@@ -0,0 +1,89 @@
+#pragma once
+
+#ifndef SIPL_IMPROC_MORPHOLOGY_H
+#define SIPL_IMPROC_MORPHOLOGY_H
+
+#include "improc/Filter.hpp"
+#include "matrix/Matrix"
+#include <limits>
+
+namespace sipl
+{
+
+namespace morphology
+{
+
+// Create kernels of various types
+namespace kernels
+{
+
+MatrixXb rectangle(size_t width, size_t height)
+{
+    assert(width % 2 == 1 && height % 2 == 1 && "width and height must be odd");
+    return MatrixXb::Ones(height, width);
+}
+}
+
+// Note: Assumes img is a binary image
+template <typename Dtype>
+MatrixX<Dtype> erode(const MatrixX<Dtype>& img, const MatrixXb& element)
+{
+    assert(element.dims[0] % 2 == 1 && element[1] % 2 == 1 &&
+           "element dimensions must be odd");
+    MatrixX<Dtype> new_img(img.dims);
+    int32_t rheight = element.dims[0] / 2;
+    int32_t rwidth = element.dims[1] / 2;
+    for (int32_t i = 0; i < img.dims[0]; ++i) {
+        for (int32_t j = 0; j < img.dims[1]; ++j) {
+            const auto patch = img.patch(i, j, rheight, rwidth);
+
+            // Compute the erosion for this patch
+            bool flag = true;
+            for (size_t e = 0; e < patch.size(); ++e) {
+                if (element[e] && !patch[e]) {
+                    flag = false;
+                    break;
+                }
+            }
+
+            // Assign result
+            new_img(i, j) = (flag ? std::numeric_limits<Dtype>::max() : 0);
+        }
+    }
+
+    return new_img;
+}
+
+// Note: Assumes img is a binary image
+template <typename Dtype>
+MatrixX<Dtype> dilate(const MatrixX<Dtype>& img, const MatrixXb& element)
+{
+    assert(element.dims[0] % 2 == 1 && element[1] % 2 == 1 &&
+           "element dimensions must be odd");
+    MatrixX<Dtype> new_img(img.dims);
+    int32_t rheight = element.dims[0] / 2;
+    int32_t rwidth = element.dims[1] / 2;
+    for (int32_t i = 0; i < img.dims[0]; ++i) {
+        for (int32_t j = 0; j < img.dims[1]; ++j) {
+            const auto patch = img.patch(i, j, rheight, rwidth);
+
+            // Compute the erosion for this patch
+            bool flag = false;
+            for (size_t e = 0; e < patch.size(); ++e) {
+                if (element[e] && patch[e]) {
+                    flag = true;
+                    break;
+                }
+            }
+
+            // Assign result
+            new_img(i, j) = (flag ? std::numeric_limits<Dtype>::max() : 0);
+        }
+    }
+
+    return new_img;
+}
+}
+}
+
+#endif
