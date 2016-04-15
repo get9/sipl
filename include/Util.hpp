9b07a3cd48644549e489424a4a70db894a960098
Sean Karlage <skarlage@get9.io>
2016-04-15 19:21:51 -0400

    Add function to print vectors

--- include/Util.hpp
+++ include/Util.hpp
@@ -3,7 +3,10 @@
 #ifndef SIPL_UTIL_H
 #define SIPL_UTIL_H
 
+#include <cmath>
+#include <iostream>
 #include <limits>
+#include <vector>
 
 namespace sipl
 {
@@ -15,6 +18,13 @@ inline double rad2deg(double rad) { return rad * 180.0 / M_PI; }
 
 inline double deg2rad(double deg) { return deg * M_PI / 180.0; }
 }
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream& s, const std::vector<T>& v)
+{
+    std::copy(std::begin(v), std::end(v), std::ostream_iterator<T>(s, "\n"));
+    return s;
 }
 
 #endif
